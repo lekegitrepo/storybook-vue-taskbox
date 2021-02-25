73746f255aea77f6a8b34ef98192f4f12ed0f013
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-25 09:18:57 +0100

    update the TaskList.spec to PureTaskList.spec and fix the example test error

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,9 +3,9 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "This is main view";
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+      propsData: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
   });
