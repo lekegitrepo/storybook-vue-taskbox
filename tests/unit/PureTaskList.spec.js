1b7ec8e5dd5f5c78a82b275a027f03bfb4c1eaa0
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-03-02 17:50:15 +0100

    fix with prettier

--- tests/unit/PureTaskList.spec.js
+++ tests/unit/PureTaskList.spec.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import PureTaskList from "../../src/components/PureTaskList.vue";
+import Vue from 'vue';
+import PureTaskList from '../../src/components/PureTaskList.vue';
 //👇 Our story imported here
-import { WithPinnedTasks } from "../../src/components/PureTaskList.stories";
+import { WithPinnedTasks } from '../../src/components/PureTaskList.stories';
 
-it("renders pinned tasks at the start of the list", () => {
+it('renders pinned tasks at the start of the list', () => {
   // render Tasklist
   const Constructor = Vue.extend(PureTaskList);
   const vm = new Constructor({
@@ -11,7 +11,7 @@ it("renders pinned tasks at the start of the list", () => {
     propsData: WithPinnedTasks.args,
   }).$mount();
   const firstTaskPinned = vm.$el.querySelector(
-    ".list-item:nth-child(1).TASK_PINNED"
+    '.list-item:nth-child(1).TASK_PINNED'
   );
 
   // We expect the pinned task to be rendered first, not at the end
